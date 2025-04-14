4cf675603688e4332b655e2c96eaa93ac33a5360
1 <Mohamed.Nashaat@code81.com>
2025-04-14 07:08:09 +0200

    04 Service Manager

--- E-Commerce.Web1/Program.cs
+++ E-Commerce.Web1/Program.cs
@@ -4,6 +4,8 @@ using Microsoft.EntityFrameworkCore;
 using Persistence;
 using Persistence.Data;
 using Persistence.Repositories;
+using Services;
+using ServicesAbstractions;
 
 namespace E_Commerce.Web1
 {
@@ -25,6 +27,7 @@ namespace E_Commerce.Web1
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
             builder.Services.AddAutoMapper(typeof(Services.AssemblyReference).Assembly);
+            builder.Services.AddScoped<IServiceManager,ServiceManager>();
 
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
--- Infrastructure/Presentation/Controllers/ProductController.cs
+++ Infrastructure/Presentation/Controllers/ProductController.cs
@@ -1,10 +1,17 @@
-﻿namespace Presentation.Controllers
+﻿using ServicesAbstractions;
+
+namespace Presentation.Controllers
 {
-    internal class ProductController
+    internal class ProductController(IServiceManager serviceManager)
     {
         // Get all products => IEnumerable<Product> 
         // Get product by id
         // Get All Brands
         // Get All Types
+
+        public void Get()
+        {
+            
+        }
     }
 }
