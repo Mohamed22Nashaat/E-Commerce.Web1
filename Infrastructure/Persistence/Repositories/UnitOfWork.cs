0284664b2434bbf6ad6d3e6877c0aaa30150c9ce
1 <Mohamed.Nashaat@code81.com>
2025-04-14 05:26:10 +0200

    08 Unit Of Work

---
+++ Infrastructure/Persistence/Repositories/UnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Persistence.Repositories
+{
+    public class UnitOfWork(StoreDbContext context) 
+        : IUnitOfWork
+    {
+        private readonly Dictionary<string, object> _repositories = [];
+        public async Task<int> SaveChangesAsync() =>
+           await context.SaveChangesAsync();
+        public IGenericRepository<TEntity, TKey> GetRepository<TEntity, TKey>() where TEntity : BaseEntity<TKey>
+        {
+            var typeName = typeof(TEntity).Name;
+            if (_repositories.ContainsKey(typeName))
+            {
+                return (IGenericRepository<TEntity, TKey>)_repositories[typeName];
+            }
+
+            var repo = new GenericRepository<TEntity, TKey>(context);
+            _repositories[typeName] = repo;
+            return repo;
+        }
+    }
+}
+//unitofwork.GetRepository<Product, int>();=> GenericRepository<Product, int>  
