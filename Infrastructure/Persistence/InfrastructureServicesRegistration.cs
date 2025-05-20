7c7fdc7880969fc7541d3209afce8225bda28555
1 <Mohamed.Nashaat@code81.com>
2025-05-20 12:03:26 +0300

    04 Basket Repository

--- Infrastructure/Persistence/InfrastructureServicesRegistration.cs
+++ Infrastructure/Persistence/InfrastructureServicesRegistration.cs
@@ -1,11 +1,4 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Persistence.Repositories;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace Persistence
 {
---
+++ Infrastructure/Persistence/Repositories/BasketRepository.cs
@@ -0,0 +1,33 @@
+﻿
+using StackExchange.Redis;
+
+namespace Persistence.Repositories
+{
+    public class BasketRepository(IConnectionMultiplexer connectionMultiplexer)
+        : IBasketRepository
+    {
+        private readonly IDatabase _database = connectionMultiplexer.GetDatabase();
+        public async Task<bool> DeleteAsync(string id) => await _database.KeyDeleteAsync(id);
+
+        public async Task<CustomerBasket?> GetAsync(string id)
+        {
+            // Get object from database
+            // Deseriliaztion
+            // Return
+            var basket = await _database.StringGetAsync(id);
+
+            if (basket.IsNullOrEmpty)
+                return null;
+
+                return JsonSerializer.Deserialize<CustomerBasket?>(basket!); 
+        }
+
+        // Used for Create & Update
+        public async Task<CustomerBasket> UpdateAsync(CustomerBasket basket, TimeSpan? timeToLive = null)
+        {
+            var jsonBasket = JsonSerializer.Serialize(basket);
+            var isCreatedOrUpdated =  await _database.StringSetAsync(basket.Id, jsonBasket, timeToLive ?? TimeSpan.FromDays(30));
+            return isCreatedOrUpdated ? await GetAsync(basket.Id) : null;
+        }
+    }
+}
